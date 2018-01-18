209ffedaa5f81c610b935436e96f170b4a8c0fcb
paramest.wsw <paramest.wsw@gmail.com>
2018-01-18 20:52:10 +0700

    test bot

--- index.js
+++ index.js
@@ -20,9 +20,45 @@ app.use('/', function(req, res, next){
 });
 
 app.post('/webhook', (req, res, next) => {
-    console.log(req);
+    // console.log(req);
+    var text = req.body.events[0].message.text
+    var sender = req.body.events[0].source.userId
+    var replyToken = req.body.events[0].replyToken
+
+    console.log(text, sender, replyToken)
+
+    if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+        sendText(sender, text)
+    }
+
     res.sendStatus(200)
 })
+
+function sendText (sender, text) {
+    let data = {
+      to: sender,
+      messages: [
+        {
+          type: 'text',
+          text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞'
+        }
+      ]
+    }
+    request({
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer key Api'
+      },
+      url: 'https://api.line.me/v2/bot/message/push',
+      method: 'POST',
+      body: data,
+      json: true
+    }, function (err, res, body) {
+      if (err) console.log('error')
+      if (res) console.log('success')
+      if (body) console.log(body)
+    });
+}
 
 app.listen(app.get('port'), function(){
     console.log('run at port :', app.get('port'));
