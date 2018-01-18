c75cb50edf5a60010aa2c7bccef33de19bf78816
paramest.wsw <paramest.wsw@gmail.com>
2018-01-18 20:19:29 +0700

    add bot

---
+++ index.js
@@ -0,0 +1,24 @@
+let express    = require('express');
+let bodyParser = require('body-parser');
+let cors       = require('cors');
+let app        = express();
+
+// fix for Cross-origin resource sharing 
+app.use(cors());
+
+// set port listen 
+app.set('port',(process.env.PORT||3000));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.use('/', function(req, res, next){
+    res.send('hello');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('run at port :', app.get('port'));
+});
