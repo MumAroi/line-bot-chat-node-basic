fc456fa7cda7fdf2a46aca8b0de967c4f3e08bc0
paramest.wsw <paramest.wsw@gmail.com>
2018-01-19 20:06:44 +0700

    test app

--- index.js
+++ index.js
@@ -1,66 +1,53 @@
-let express    = require('express');
-let bodyParser = require('body-parser');
-let cors       = require('cors');
-var request    = require('request');
-let app        = express();
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
 
-// fix for Cross-origin resource sharing 
-app.use(cors());
+app.use(bodyParser.json())
 
-// set port listen 
-app.set('port',(process.env.PORT||3000));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}));
-// parse application/json
-app.use(bodyParser.json());
-
-
-app.use('/', function(req, res){
-    res.send('hello');
-});
+app.set('port', (process.env.PORT || 4000))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.post('/webhook', (req, res) => {
-    // console.log(req);
-    var text       = req.body.events[0].message.text
-    var sender     = req.body.events[0].source.userId
-    var replyToken = req.body.events[0].replyToken
-    console.log('this data');
-    console.log(text, sender, replyToken);
-    
-    if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-        sendText(sender, text)
-    }
-
-    res.sendStatus(200)
+  var text = req.body.events[0].message.text
+  var sender = req.body.events[0].source.userId
+  var replyToken = req.body.events[0].replyToken
+  console.log(text, sender, replyToken)
+  console.log(typeof sender, typeof text)
+  // console.log(req.body.events[0])
+  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+    sendText(sender, text)
+  }
+  res.sendStatus(200)
 })
 
 function sendText (sender, text) {
-    let data = {
-      to: sender,
-      messages: [
-        {
-          type: 'text',
-          text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞'
-        }
-      ]
-    };
-    request({
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer key Api'
-      },
-      url: 'https://api.line.me/v2/bot/message/push',
-      method: 'POST',
-      body: data,
-      json: true
-    }, function (err, res, body) {
-      if (err) console.log('error')
-      if (res) console.log('success')
-      if (body) console.log(body)
-    });
+  let data = {
+    to: sender,
+    messages: [
+      {
+        type: 'text',
+        text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞'
+      }
+    ]
+  }
+  request({
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer key Api'
+    },
+    url: 'https://api.line.me/v2/bot/message/push',
+    method: 'POST',
+    body: data,
+    json: true
+  }, function (err, res, body) {
+    if (err) console.log('error')
+    if (res) console.log('success')
+    if (body) console.log(body)
+  })
 }
 
-app.listen(app.get('port'), function(){
-    console.log('run at port :', app.get('port'));
-});
+app.listen(app.get('port'), function () {
+  console.log('run at port', app.get('port'))
+})
