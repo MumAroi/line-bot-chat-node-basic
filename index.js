4ef59e0c07de5c0d42cee091ba89d7759409d899
paramest.wsw <paramest.wsw@gmail.com>
2018-01-19 20:20:06 +0700

    test code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
 
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
 app.set('port', (process.env.PORT || 4000))
 app.use(bodyParser.urlencoded({extended: true}))
